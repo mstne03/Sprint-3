cc50a7ef49896dc74deeba12b412532cffe422be
mstne03 <mserraitorne@gmail.com>
2025-06-04 10:07:58 +0200

    First commit

--- src/films.js
+++ src/films.js
@@ -1,13 +1,11 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
-  return result;
+  
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
@@ -21,26 +19,24 @@ function orderAlphabetically(array) {
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
-}
-
-// Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
-
-}
-
-// Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
-
-}
-
-// Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
+function orderByYear(array) {
   
 }
 
+// Exercise 6: Calculate the average of the movies in a category
+function moviesAverageByCategory(array, category) {
+  
+}
 
+// Exercise 7: Modify the duration of movies to minutes
+function hoursToMinutes(array) {
+  
+}
+
+// Exercise 8: Get the best film of a year
+function bestFilmOfYear(array, year) {
+
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
